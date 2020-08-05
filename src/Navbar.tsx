f6af245eb889b0b68fa651c09e5ced0089e7f336
Daniel Gretzke <daniel@gretzke.de>
2020-08-05 10:57:19 +0200

    fixed navbar links

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -1,20 +1,28 @@
 import * as React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 export class MyNavbar extends React.Component<{}, {}> {
   public render() {
     return (
-      <>
-        <Navbar bg="light" variant="light">
-          <Navbar.Brand href="#home">inacta Tezos Gateway</Navbar.Brand>
-          <Nav className="mr-auto">
-            <Nav.Link href="/">Balances</Nav.Link>
-            <Nav.Link href="/wallet">Wallet</Nav.Link>
-            <Nav.Link href="/contract-deployment">Contract Deployment</Nav.Link>
-            <Nav.Link href="/secret-key-conversion">Key Conversion</Nav.Link>
-          </Nav>
-        </Navbar>
-      </>
+      <Navbar bg="light" variant="light">
+        <Navbar.Brand>Tezos TokenGate</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">
+            Balances
+          </Nav.Link>
+          <Nav.Link as={Link} to="/wallet">
+            Wallet
+          </Nav.Link>
+          <Nav.Link as={Link} to="/contract-deployment">
+            Contract Deployment
+          </Nav.Link>
+          <Nav.Link as={Link} to="/secret-key-conversion">
+            Key Conversion
+          </Nav.Link>
+        </Nav>
+      </Navbar>
     );
   }
 }
