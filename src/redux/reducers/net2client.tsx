960bbf2904fe24c61dc0d8fea93ca408a58ed902
Daniel Gretzke <daniel@gretzke.de>
2020-08-18 17:16:13 +0200

    automatically set signer when adding a new wallet

--- src/redux/reducers/net2client.tsx
+++ src/redux/reducers/net2client.tsx
@@ -1,5 +1,6 @@
 import { EnumDictionary } from '../../shared/AbstractTypes';
 import { Net } from '../../shared/TezosTypes';
+import { InMemorySigner } from '@taquito/signer';
 import { TezosToolkit } from '@taquito/taquito';
 
 const initialState: EnumDictionary<Net, TezosToolkit> = {
@@ -8,27 +9,23 @@ const initialState: EnumDictionary<Net, TezosToolkit> = {
 };
 
 export default function(state = initialState, action) {
-  if (action.type === 'SET_PROVIDER') {
-    state[action.network].setProvider({
-      rpc: action.rpc
-    });
+  if (action.type === 'SET_RPC_PROVIDER') {
+    state[action.network].setRpcProvider(action.rpc);
+  } else if (action.type === 'SET_SIGNER') {
+    state[action.network].setSignerProvider(new InMemorySigner(action.key));
   } else if (action.type === 'RESET_TOOLKIT') {
     if (action.network === Net.Mainnet) {
       state = {
         [Net.Mainnet]: new TezosToolkit(),
         [Net.Testnet]: state[Net.Testnet]
       };
-      state[Net.Mainnet].setProvider({
-        rpc: 'https://mainnet-tezos.giganode.io'
-      });
+      state[Net.Mainnet].setRpcProvider('https://mainnet-tezos.giganode.io');
     } else {
       state = {
         [Net.Mainnet]: state[Net.Mainnet],
         [Net.Testnet]: new TezosToolkit()
       };
-      state[Net.Testnet].setProvider({
-        rpc: 'https://api.tez.ie/rpc/carthagenet'
-      });
+      state[Net.Testnet].setRpcProvider('https://api.tez.ie/rpc/carthagenet');
     }
   }
   return state;
