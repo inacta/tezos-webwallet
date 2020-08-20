2cbfa013c04ac37594e4ec698894d2c057d45e69
Daniel Gretzke <daniel@gretzke.de>
2020-08-20 17:19:27 +0200

    moved to functional component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,14 +2,12 @@ import './Header.scss';
 import * as React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 
-export class Header extends React.Component<{}, {}> {
-  public render() {
-    return (
-      <Navbar bg="dark" variant="dark" className="justify-content-between">
-        <Navbar.Brand>
-          <img className="Header-logo" src="/assets/img/logo.png" alt="TokenGate Tezos" />
-        </Navbar.Brand>
-      </Navbar>
-    );
-  }
+export default function Header() {
+  return (
+    <Navbar bg="dark" variant="dark" className="justify-content-between">
+      <Navbar.Brand>
+        <img className="Header-logo" src="/assets/img/logo.png" alt="TokenGate Tezos" />
+      </Navbar.Brand>
+    </Navbar>
+  );
 }
