8acf3b428c5216d9be7ea1b490c4b06ca521c7f9
Daniel Gretzke <daniel@gretzke.de>
2020-08-21 12:03:58 +0200

    fixed styling issue

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -10,26 +10,22 @@ export default function Loading(props: ILoadingProps) {
   const loadingIcon = (
     <div
       className="d-flex flex-column justify-content-center h-100"
-      style={{ minWidth: props.large ? '200px' : '30px' }}
+      style={{ minWidth: props.large ? '150px' : '30px' }}
     >
-      <img src="/assets/img/logo_animated.svg" style={{ maxHeight: props.large ? '200px' : '30px' }} alt="" />
+      <img src="/assets/img/logo_animated.svg" style={{ maxHeight: props.large ? '150px' : '30px' }} alt="" />
       {props.children === undefined ? (
         <div></div>
       ) : props.large ? (
-        <h1 className="align-self-center mt-3">{props.children}</h1>
+        <h2 className="align-self-center mt-3">{props.children}</h2>
       ) : (
         <span className="align-self-center mt-1">{props.children}</span>
       )}
     </div>
   );
 
-  return (
-    <div>
-      {props.center === undefined || props.center ? (
-        loadingIcon
-      ) : (
-        <div className={'d-flex h-100 justify-content-start py-2'}>{loadingIcon}</div>
-      )}
-    </div>
+  return props.center === undefined || props.center ? (
+    loadingIcon
+  ) : (
+    <div className={'d-flex justify-content-start py-2'}>{loadingIcon}</div>
   );
 }
