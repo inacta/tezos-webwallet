382c1eb0d0292b28239a0a439425112ed5b9ddd0
Thorkil Vaerge <thorkil.vaerge@inacta.ch>
2020-11-23 20:13:39 +0100

    Improve admin UI for registering tandems
    Previously multiple helpers/helpees/activities had to be input as
    comma-separated lists, now they have a field each.
    
    This commit also ensures that helper/helpee/activity values are not
    repeated within the same tandem as this cannot be converted to a
    Michelson set in the deployed smart contract.

--- src/components/TokenManagement/KissModalAdmin.tsx
+++ src/components/TokenManagement/KissModalAdmin.tsx
@@ -9,13 +9,14 @@ import Modal from 'react-bootstrap/Modal';
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import { addNotification } from '../../shared/NotificationService';
+import clonedeep from 'lodash.clonedeep';
 import { isValidAddress } from '../../shared/TezosUtil';
 import { registerTandemAdminClaim } from '../../shared/TezosService';
 
 interface IKissAdminClaimsInput {
-  activities: string;
-  helpees: string;
-  helpers: string;
+  activities: string[];
+  helpees: string[];
+  helpers: string[];
   minutes: string;
 }
 
@@ -36,18 +37,19 @@ interface IKissModalAdminState {
 }
 
 export class KissModalAdmin extends React.Component<IKissModalAdminProps, IKissModalAdminState> {
+  // Since this is a deeply nested object, only its deepclone copies should be used since
+  // fields will be repeated across different tandem claims if that is not the case
   private emptyClaimInput: IKissAdminClaimsInput = {
-    activities: '',
-    helpees: '',
-    helpers: '',
+    activities: [''],
+    helpees: [''],
+    helpers: [''],
     minutes: ''
   };
 
   private constructor(props: IKissModalAdminProps) {
     super(props);
-    const emptyClaim = { ...this.emptyClaimInput };
+    const emptyClaim = clonedeep(this.emptyClaimInput);
     this.state = {
-      // claims: [] as IKissTandemAdminClaim[],
       claims: [emptyClaim],
       calculatingFee: false,
       fee: 0,
@@ -55,27 +57,69 @@ export class KissModalAdmin extends React.Component<IKissModalAdminProps, IKissM
     };
   }
 
+  // Used since several input fields are sets and it does not accept repeated values
+  private hasNonEmptyDuplicates(xs: string[]): boolean {
+    return xs.some((item, index) => item !== '' && xs.indexOf(item) !== index);
+  }
+
   // The spread operator creates copies of the arrays and objects
   // so multiple entries in the claims array do not reference the same object instance.
   private updateField(
     event: React.ChangeEvent<HTMLInputElement>,
-    i: number,
-    type: 'helpers' | 'helpees' | 'activities' | 'minutes'
+    tandemIndex: number,
+    otherIndex: number,
+    type: 'helpers' | 'helpees' | 'activities'
   ) {
     const newValue = event.currentTarget.value;
     const newArray = [...this.state.claims];
-    newArray[i][type] = newValue;
+    newArray[tandemIndex][type][otherIndex] = newValue;
     this.setState({ claims: newArray });
   }
 
-  private addField(): void {
+  private updateMinutes(event: React.ChangeEvent<HTMLInputElement>, tandemIndex: number) {
+    const newValue = event.currentTarget.value;
     const newArray = [...this.state.claims];
-    const newClaim = { ...this.emptyClaimInput };
+    newArray[tandemIndex]['minutes'] = newValue;
+    this.setState({ claims: newArray });
+  }
+
+  private addElementToTandemField(
+    event: React.MouseEvent<HTMLElement>,
+    tandemIndex: number,
+    type: 'helpers' | 'helpees' | 'activities'
+  ): void {
+    event.preventDefault();
+    const claims = [...this.state.claims];
+    const newArray = [...this.state.claims[tandemIndex][type]];
+    const newHelper = '';
+    newArray.push(newHelper);
+    claims[tandemIndex][type] = newArray;
+    this.setState({ claims });
+  }
+
+  private removeElementFromTandemField(
+    event: React.MouseEvent<HTMLElement>,
+    tandemIndex: number,
+    elementIndex: number,
+    type: 'helpers' | 'helpees' | 'activities'
+  ): void {
+    event.preventDefault();
+    const claims = [...this.state.claims];
+    const newArray = [...this.state.claims[tandemIndex][type]];
+    newArray.splice(elementIndex, 1);
+    claims[tandemIndex][type] = newArray;
+    this.setState({ claims });
+  }
+
+  private addTandemClaim(): void {
+    const newArray = [...this.state.claims];
+    const newClaim = clonedeep(this.emptyClaimInput);
     newArray.push(newClaim);
     this.setState({ claims: newArray });
   }
 
-  private removeField(i: number): void {
+  private removeTandemClaim(e: React.MouseEvent<HTMLElement>, i: number): void {
+    e.preventDefault();
     const newArray = [...this.state.claims];
     newArray.splice(i, 1);
     this.setState({ claims: newArray });
@@ -84,9 +128,9 @@ export class KissModalAdmin extends React.Component<IKissModalAdminProps, IKissM
   private async handleSubmitHelper() {
     const adminClaims: IKissTandemAdminClaim[] = this.state.claims.map(function(x) {
       return {
-        activities: x.activities.split(',').map((y: string) => parseInt(y, 10)),
-        helpees: x.helpees.split(','),
-        helpers: x.helpers.split(','),
+        activities: x.activities.map((y: string) => parseInt(y, 10)),
+        helpees: x.helpees,
+        helpers: x.helpers,
         minutes: parseInt(x.minutes, 10)
       };
     });
@@ -119,20 +163,18 @@ export class KissModalAdmin extends React.Component<IKissModalAdminProps, IKissM
   }
 
   private reset() {
-    const emptyClaim = { ...this.emptyClaimInput };
+    const emptyClaim = clonedeep(this.emptyClaimInput);
     this.setState({ claims: [emptyClaim] as IKissAdminClaimsInput[], calculatingFee: false, fee: 0, loading: false });
   }
 
-  private validAddressInput(helpersValue: string): boolean {
-    const helpers = helpersValue.replace(/\s/g, '').split(',');
+  private validAddressInput(input: string): boolean {
+    if (!input) return false;
 
-    return helpers.length > 0 && helpers.every((x) => isValidAddress(x));
+    return isValidAddress(input);
   }
 
   private validActivitiesInput(activitiesValue: string): boolean {
-    const activities = activitiesValue.replace(/\s/g, '').split(',');
-
-    return activities.length > 0 && activities.every((x) => !isNaN(parseInt(x)));
+    return !isNaN(parseInt(activitiesValue));
   }
 
   private validateMinutesInput(minuteValue: string): boolean {
@@ -142,70 +184,149 @@ export class KissModalAdmin extends React.Component<IKissModalAdminProps, IKissM
   }
 
   public render() {
-    const validHelpersInput: boolean[] = this.state.claims.map((x) => this.validAddressInput(x.helpers));
-    const validHelpeesInput: boolean[] = this.state.claims.map((x) => this.validAddressInput(x.helpees));
-    const validActivitiesInput: boolean[] = this.state.claims.map((x) => this.validActivitiesInput(x.activities));
+    const validHelpersInput: boolean[][] = this.state.claims.map((x) =>
+      x.helpers.map((y) => this.validAddressInput(y))
+    );
+    const validHelpeesInput: boolean[][] = this.state.claims.map((x) =>
+      x.helpees.map((y) => this.validAddressInput(y))
+    );
+    const validActivitiesInput: boolean[][] = this.state.claims.map((x) =>
+      x.activities.map((y) => this.validActivitiesInput(y))
+    );
     const validMinutesInput: boolean[] = this.state.claims.map((x) => this.validateMinutesInput(x.minutes));
+
+    // Duplicates in the inputes are disallowed since sets are used in the smart contract and they
+    // do not allow repeated values
+    const hasHelpeeDuplicates: boolean[] = this.state.claims.map((x) => this.hasNonEmptyDuplicates(x.helpees));
+    const hasHelperDuplicates: boolean[] = this.state.claims.map((x) => this.hasNonEmptyDuplicates(x.helpers));
+    const hasActivityDuplicates: boolean[] = this.state.claims.map((x) => this.hasNonEmptyDuplicates(x.activities));
     const validInputs: boolean =
-      validHelpersInput.every((x) => x) &&
-      validHelpersInput.every((x) => x) &&
-      validActivitiesInput.every((x) => x) &&
-      validMinutesInput.every((x) => x);
+      validHelpersInput.every((x) => x.every((y) => y)) &&
+      validHelpeesInput.every((x) => x.every((y) => y)) &&
+      validActivitiesInput.every((x) => x.every((y) => y)) &&
+      validMinutesInput.every((x) => x) &&
+      hasHelpeeDuplicates.every((x) => !x) &&
+      hasHelperDuplicates.every((x) => !x) &&
+      hasActivityDuplicates.every((x) => !x);
     const inputs: JSX.Element = (
       <>
         {this.state.claims.map((field, i) => (
           <>
             <Form.Row key={i}>
               <Form.Group as={Col} xs="8" controlId={'helpers-' + i}>
-                <Form.Control
-                  type="text"
-                  className={`font-weight-bold ${!validHelpersInput[i] && 'is-invalid'}`}
-                  placeholder="Helpers"
-                  value={field.helpers}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateField(e, i, 'helpers')}
-                  required
-                ></Form.Control>
+                Helpers
+                {field.helpers.map((helper, j) => (
+                  <Form.Control
+                    key={j}
+                    type="text"
+                    className={`font-weight-bold ${!validHelpersInput[i][j] && 'is-invalid'}`}
+                    placeholder="Helper"
+                    value={helper}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateField(e, i, j, 'helpers')}
+                    required
+                  ></Form.Control>
+                ))}
+                {hasHelperDuplicates[i] && <small className="text-danger">Duplicates detected</small>}
+                <IconButton
+                  onClick={(e: React.MouseEvent<HTMLElement>) => this.addElementToTandemField(e, i, 'helpers')}
+                >
+                  <FaPlusCircle color="blue" />
+                </IconButton>
+                {field.helpers.length > 1 ? (
+                  <IconButton
+                    onClick={(e: React.MouseEvent<HTMLElement>) =>
+                      this.removeElementFromTandemField(e, i, field.helpers.length - 1, 'helpers')
+                    }
+                  >
+                    <FaMinusCircle color="red" />
+                  </IconButton>
+                ) : (
+                  undefined
+                )}
               </Form.Group>
               <Form.Group as={Col} xs="3" controlId={'activities-key-' + i}>
-                <Form.Control
-                  type="text"
-                  className={`font-weight-bold ${!validActivitiesInput[i] && 'is-invalid'}`}
-                  placeholder="Activities"
-                  value={field.activities}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateField(e, i, 'activities')}
-                  required
-                ></Form.Control>
+                Activities
+                {field.activities.map((activity, j) => (
+                  <Form.Control
+                    key={j}
+                    type="text"
+                    className={`font-weight-bold ${!validActivitiesInput[i][j] && 'is-invalid'}`}
+                    placeholder="Activity"
+                    value={activity}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateField(e, i, j, 'activities')}
+                    required
+                  ></Form.Control>
+                ))}
+                {hasActivityDuplicates[i] && <small className="text-danger">Duplicates detected</small>}
+                <IconButton
+                  onClick={(e: React.MouseEvent<HTMLElement>) => this.addElementToTandemField(e, i, 'activities')}
+                >
+                  <FaPlusCircle color="blue" />
+                </IconButton>
+                {field.activities.length > 1 ? (
+                  <IconButton
+                    onClick={(e: React.MouseEvent<HTMLElement>) =>
+                      this.removeElementFromTandemField(e, i, field.activities.length - 1, 'activities')
+                    }
+                  >
+                    <FaMinusCircle color="red" />
+                  </IconButton>
+                ) : (
+                  undefined
+                )}
               </Form.Group>
               <Col xs="1">
-                <IconButton onClick={() => this.removeField(i)}>
+                <IconButton onClick={(e: React.MouseEvent<HTMLElement>) => this.removeTandemClaim(e, i)}>
                   <FaMinusCircle />
                 </IconButton>
               </Col>
             </Form.Row>
             <Form.Row key={`${i}-b`}>
               <Form.Group as={Col} xs="8" controlId={'helpees-' + i}>
-                <Form.Control
-                  type="text"
-                  className={`font-weight-bold ${!validHelpeesInput[i] && 'is-invalid'}`}
-                  placeholder="Helpees"
-                  value={field.helpees}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateField(e, i, 'helpees')}
-                  required
-                ></Form.Control>
+                Helpees
+                {field.helpees.map((helpee, j) => (
+                  <Form.Control
+                    key={j}
+                    type="text"
+                    className={`font-weight-bold ${!validHelpeesInput[i][j] && 'is-invalid'}`}
+                    placeholder="Helpee"
+                    value={helpee}
+                    onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateField(e, i, j, 'helpees')}
+                    required
+                  ></Form.Control>
+                ))}
+                {hasHelpeeDuplicates[i] && <small className="text-danger">Duplicates detected</small>}
+                <IconButton
+                  onClick={(e: React.MouseEvent<HTMLElement>) => this.addElementToTandemField(e, i, 'helpees')}
+                >
+                  <FaPlusCircle color="blue" />
+                </IconButton>
+                {field.helpees.length > 1 ? (
+                  <IconButton
+                    onClick={(e: React.MouseEvent<HTMLElement>) =>
+                      this.removeElementFromTandemField(e, i, field.helpees.length - 1, 'helpees')
+                    }
+                  >
+                    <FaMinusCircle color="red" />
+                  </IconButton>
+                ) : (
+                  undefined
+                )}
               </Form.Group>
               <Form.Group as={Col} xs="3" controlId={'minutes-value-' + i}>
+                Minutes
                 <Form.Control
                   type="text"
                   className={`font-weight-bold ${!validMinutesInput[i] && 'is-invalid'}`}
                   placeholder="Minutes"
                   value={field.minutes}
-                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateField(e, i, 'minutes')}
+                  onChange={(e: React.ChangeEvent<HTMLInputElement>) => this.updateMinutes(e, i)}
                   required
                 ></Form.Control>
               </Form.Group>
               <Col xs="1"></Col>
             </Form.Row>
-            <br />
+            <hr />
           </>
         ))}
       </>
@@ -229,7 +350,7 @@ export class KissModalAdmin extends React.Component<IKissModalAdminProps, IKissM
           <Form noValidate validated={validInputs} id="register-tandem-admin-form">
             {inputs}
           </Form>
-          <IconButton onClick={() => this.addField()}>
+          <IconButton onClick={() => this.addTandemClaim()}>
             <FaPlusCircle />
           </IconButton>
         </Modal.Body>
